e5836970021ab4faab7cc4bdc8fe6ae753afb186
zinx110 <tasdid110@gmail.com>
2024-11-17 15:37:56 +0600

    directed the pipeline to remove fs

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const { ytdown } = require("nayan-media-downloader")
 const { initializeApp, cert } = require("firebase-admin/app");
-
 const admin = require('firebase-admin');
-const ytdl = require("@distube/ytdl-core");
-const fs = require("fs");
-const path = require("path");
-const youtubedl = require('youtube-dl-exec')
 const { https } = require('follow-redirects');
+
+
 // Initialize Firebase
 const serviceAccount = require("./.secrets/serviceAccountKey.json");
 initializeApp({
@@ -26,33 +23,13 @@ async function downloadAndUpload(youtubeURL, uploadPath) {
 
      https.get(downloadURL, (response) => {
         if (response.statusCode === 200) {
-
-
-
-            const fileStream = fs.createWriteStream("abc.mp4");
-            const firebaseStream = file.createWriteStream()
-
-
-
-            response.pipe(fileStream);
-
+  
+            const firebaseStream = file.createWriteStream({contentType:"video/mp4"})
+            response.pipe(firebaseStream);
 
             firebaseStream.on("finish",()=>console.log("Uploading in firebase done"))
-            fileStream.on('finish', () => {
-                console.log('Download completed!');
-                bucket.upload("abc.mp4",{
-                    destination:uploadPath,
-                }, (err0r,file)=>{
-                    if(err0r){
-                        console.log("error downloading")
-                    }else{
-                        console.log("uploaded successfully")
-                    }
-
-                })
-            });
-
-            fileStream.on('error', (error) => {
+            
+            firebaseStream.on('error', (error) => {
                 console.error('Error during download:', error);
             });
         } else {
@@ -70,5 +47,5 @@ async function downloadAndUpload(youtubeURL, uploadPath) {
 // Example usage
 downloadAndUpload(
   "https://www.youtube.com/watch?v=FAyKDaXEAgc",
-  "uploaded_video/sample1.mp4"
+  "uploaded_video/vid2.mp4"
 );
